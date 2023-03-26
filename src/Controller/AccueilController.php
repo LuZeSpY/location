59bc0cc6cc1e2f126ca65da8d985f71e6311f508
Lucas Didier <lucas.didier@hotmail.fr>
2023-03-26 13:35:27 +0200

    Generating Authenticator pages

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,15 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Repository\AppartementRepository;
+use App\Entity\Appartement;
+
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(AppartementRepository $appartementRepository): Response
     {
+
+        $totalAppartement = $appartementRepository->createQueryBuilder('a')
+            ->select('count(a.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'appartementTotal' => $totalAppartement,
         ]);
     }
+
 }
 
 ?>
